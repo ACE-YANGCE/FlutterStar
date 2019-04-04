d184431db0bf243cc614e9526ea89ffa07a275b5
sevewing <liweisi8121@hotmail.com>
2019-04-04 10:13:47 +0200

    merged final

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Users/yangce/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: /Users/Levis/Developer/Libraries/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- lib/main.dart
+++ lib/main.dart
@@ -7,164 +7,181 @@ import 'package:flutter/services.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  return runApp(App());
+}
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: StarPage());
+    return MaterialApp(home: Scaffold(body: Page()));
   }
 }
 
-class StarPage extends StatefulWidget {
+class Page extends StatefulWidget {
   @override
-  _StarPageState createState() => _StarPageState();
+  _St createState() => _St();
 }
 
-class _StarPageState extends State<StarPage> {
-  List<Offset> _p = [], _sp = [];
-  var name = 'Aries', _st, data, _wid, _hig;
-  Offset _ep;
+class _St extends State<Page> {
+  List<Offset> p = [];
+  var n = 'aries', st, da, _w, _h;
+  Offset o;
+  List<List<double>> s = [];
+  List l = [];
+  Set set = Set();
 
   @override
   void initState() {
     super.initState();
-    _wid = MediaQueryData.fromWindow(ui.window).size.width;
-    _hig = MediaQueryData.fromWindow(ui.window).size.height;
-    try {
-      _loadImg('images/ic_star.png').then((st) {
-        _st = st;
-      }).whenComplete(() {
-        if (this.mounted) {
-          setState(() {});
-        }
-      });
-    } catch (ex) {}
-    _loadAst().then((val) {
-      data = json.decode(val);
-      setData(data[name] as List);
+    _w = MediaQueryData.fromWindow(ui.window).size.width;
+    _h = MediaQueryData.fromWindow(ui.window).size.height;
+
+    _loadImg('img/star.png').then((s) {
+      st = s;
+    }).whenComplete(() {
+      if (this.mounted) setState(() {});
+    });
+
+    _loadAst().then((v) {
+      da = json.decode(v);
+      setData("0");
     });
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext c) {
     return Scaffold(
         body: Stack(children: <Widget>[
-      Image.asset('images/ic_bg.jpg', width: _wid, fit: BoxFit.fill),
-      Container(
-          child: CustomPaint(painter: StarCanvas(_wid, _hig, _p, _sp, _st))),
-      starWid(1, 2),
-      starWid(1, 1),
-      starWid(0, 1),
-      GestureDetector(onPanDown: (d) {
-        _p.add(Offset(d.globalPosition.dx, d.globalPosition.dy));
-        setState(() {});
-      }, onPanUpdate: (d) {
-        _ep = Offset(d.globalPosition.dx, d.globalPosition.dy);
-      }, onPanEnd: (d) {
-        _p.add(_ep);
-        setState(() {});
-      })
-    ]));
+          Image.asset('img/bg.jpg', width: _w, fit: BoxFit.fill),
+          Container(child: CustomPaint(painter: StarCanvas(_w, _h, p, s, st))),
+          wid(1, 2),
+          wid(1, 1),
+          wid(0, 1),
+          GestureDetector(onPanDown: (d) {
+            p.add(Offset(d.globalPosition.dx, d.globalPosition.dy));
+            setState(() {});
+          }, onPanUpdate: (d) {
+            o = Offset(d.globalPosition.dx, d.globalPosition.dy);
+          }, onPanEnd: (d) {
+            p.add(o);
+            setState(() {});
+
+            double sz, ez;
+            for (int i = 0; i < p.length - 1; i += 2) {
+              if (p[i] != null && p[i + 1] != null) {
+                bool st = false, en = false;
+                double w, h;
+                for (int j = 0; j < s.length; j++) {
+                  w = s[j][0] * _w;
+                  h = s[j][1] * _h;
+
+                  if ((p[i].dx - w).abs() <= 20 && (p[i].dy - h).abs() <= 20) {
+                    p[i] = Offset(w, h);
+                    sz = s[j][2];
+                    st = true;
+                  }
+                  if ((p[i + 1].dx - w).abs() <= 20 &&
+                      (p[i + 1].dy - h).abs() <= 20) {
+                    p[i + 1] = Offset(w, h);
+                    ez = s[j][2];
+                    en = true;
+                  }
+                }
+                if (!st) p[i] = null;
+                if (!en) p[i + 1] = null;
+              }
+
+              if (p[i] != null && p[i + 1] != null && p[i] != p[i + 1])
+                set..add('${sz}|${ez}')..add('${ez}|${sz}');
+            }
+            int n = 0;
+            if (set.length == l.length) {
+              for (String e in set) {
+                for (int i = 0; i < l.length; i++) if (e == l[i]) n += 1;
+              }
+            }
+            if (n > 0 && n == l.length)
+              Scaffold.of(c)
+                  .showSnackBar(SnackBar(content: Text(da['tip'].toString())));
+          })
+        ]));
   }
 
-  Widget starWid(l, b) {
+  Widget wid(l, b) {
     return Container(
-        alignment: l == 0 ? Alignment.bottomLeft : Alignment.bottomRight,
-        margin: EdgeInsets.only(
-            bottom: 16.0 + 84 * (b - 1), right: 16.0, left: 16.0),
+        alignment: Alignment(l == 0 ? -1.0 : 1.0, 1.0),
+        margin: EdgeInsets.fromLTRB(32, 0, 32, 32.0 + 84 * (b - 1)),
         child: GestureDetector(
-            child: ClipOval(
-                child: Container(
-                    color: Colors.black54,
-                    width: 56,
-                    padding: EdgeInsets.all(16),
-                    child: Image.asset(
-                        'images/ic_${l == 0 ? name.toLowerCase() : (b == 1 ? 'reset' : 'ram')}.png'))),
+            child: Container(
+                width: 24,
+                child: Image.asset(
+                    'img/${l == 0 ? n : (b == 1 ? 'reset' : 'ram')}.png')),
             onTap: () {
-              if (b == 2) {
-                List<Map> rl = (data['Random'] as List).cast();
-                name = rl[0][Random().nextInt(12).toString()];
-                setData(data[name] as List);
-              }
-              reset();
+              if (b == 2) setData(Random().nextInt(12).toString());
+              p.clear();
+              set.clear();
+              setState(() {});
             }));
   }
 
-  void reset() {
-    _p.clear();
-    setState(() {});
-  }
-
-  void setData(li) {
-    _sp.clear();
-    for (int i = 0; i < li.length; i++) {
-      _sp.add(Offset((li[i]['x']).toDouble(), li[i]['y'].toDouble()));
+  setData(r) {
+    Map map = (da['ran'] as List).cast()[0][r];
+    n = map['name'];
+    l = map['res'] as List;
+    List ls = da[n] as List;
+    s.clear();
+    for (int i = 0; i < ls.length; i++) {
+      List<double> tl = [];
+      tl..add(ls[i]['x'])..add(ls[i]['y'])..add((ls[i]['z']));
+      s.add(tl);
     }
     setState(() {});
   }
 }
 
 class StarCanvas extends CustomPainter {
-  List<Offset> _p = [], _sp = [];
-  var _st, _wid, _hig;
+  List<Offset> p = [];
+  List<List<double>> s = [];
+  var st, w, h;
 
-  StarCanvas(this._wid, this._hig, this._p, this._sp, this._st);
+  StarCanvas(this.w, this.h, this.p, this.s, this.st);
 
-  Paint _pa = Paint()
+  Paint pa = Paint()
     ..color = Colors.white
     ..strokeWidth = 2;
 
   @override
   void paint(Canvas canvas, Size size) {
-    for (int i = 0; i < _p.length - 1; i += 2) {
-      if (_p[i] != null && _p[i + 1] != null) {
-        bool st = false, en = false;
-        for (int j = 0; j < _sp.length; j++) {
-          if (_p[i].dx <= _sp[j].dx * _wid + 20 &&
-              _p[i].dx >= _sp[j].dx * _wid - 20 &&
-              _p[i].dy <= _sp[j].dy * _hig + 20 &&
-              _p[i].dy >= _sp[j].dy * _hig - 20) {
-            _p[i] = Offset(_sp[j].dx * _wid, _sp[j].dy * _hig);
-            st = true;
-          }
-          if (_p[i + 1].dx <= _sp[j].dx * _wid + 20 &&
-              _p[i + 1].dx >= _sp[j].dx * _wid - 20 &&
-              _p[i + 1].dy <= _sp[j].dy * _hig + 20 &&
-              _p[i + 1].dy >= _sp[j].dy * _hig - 20) {
-            _p[i + 1] = Offset(_sp[j].dx * _wid, _sp[j].dy * _hig);
-            en = true;
-          }
-        }
-        if (!st) _p[i] = null;
-        if (!en) _p[i + 1] = null;
-      }
-      if (_p[i] != null && _p[i + 1] != null)
-        canvas.drawLine(_p[i], _p[i + 1], _pa);
+    for (int i = 0; i < p.length - 1; i += 2) {
+      if (p[i] != null && p[i + 1] != null && p[i] != p[i + 1])
+        canvas.drawLine(p[i], p[i + 1], pa);
     }
 
     canvas.save();
-    for (int i = 0; i < _sp.length; i++) {
-      canvas.translate(_sp[i].dx * _wid, _sp[i].dy * _hig);
-      if (_st != null) canvas.drawImage(_st, Offset(-15.0, -15.0), _pa);
-      canvas.translate(-_sp[i].dx * _wid, -_sp[i].dy * _hig);
+    for (List<double> e in s) {
+      canvas.translate(e[0] * w, e[1] * h);
+      if (st != null) canvas.drawImage(st, Offset(-15.0, -15.0), pa);
+      canvas.translate(-e[0] * w, -e[1] * h);
     }
     canvas.restore();
   }
 
   @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
+  bool shouldRepaint(CustomPainter o) {
     return true;
   }
 }
 
-Future<ui.Image> _loadImg(String asset) async {
-  ByteData bd = await rootBundle.load(asset);
+Future<ui.Image> _loadImg(String s) async {
+  ByteData bd = await rootBundle.load(s);
   var co = await ui.instantiateImageCodec(bd.buffer.asUint8List());
   FrameInfo fi = await co.getNextFrame();
   return fi.image;
 }
 
 Future<String> _loadAst() async {
-  return await rootBundle.loadString('images/points.json');
+  return await rootBundle.loadString('img/p.json');
 }
